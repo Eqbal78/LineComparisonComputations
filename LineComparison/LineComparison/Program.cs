b4c4241dbb1a3414e705b1b91212b888d21c9e8a
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-02 13:21:27 +0530

    Calculating the Length of Two line

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcom to Line Comparison Computation.");
+
+
+            Console.WriteLine("Enter the value of x1 : ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1 : ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2 : ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2 : ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length_of_line = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("The Length of 2 Points(" + x1 + "," + y1 + ") and (" + x2 + "," + y2 + ") is " + length_of_line);
+        }
+    }
+}
