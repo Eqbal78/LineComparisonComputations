76a2e7c1c42e381326450f151e57b946797a16e1
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-07 21:07:09 +0530

    Using Opps Comparison Between Two Line

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,37 +1,54 @@
 ﻿using System;
 
-namespace LineComparison
+namespace practiceLineCompare
 {
-    class Program
+    public interface IComparison
     {
-        static int Compare()
+        public void CompareLine();
+    }
+
+    public class Line
+    {
+        public int x1, y1, x2, y2, x3, y3, x4, y4;
+
+        public Line()
+        {
+
+        }
+
+        public void Input()
         {
             Console.WriteLine("Welcome to Line Comparison Computation.");
 
             Console.WriteLine("Enter values x1 and y1 for Line 1");
             Console.WriteLine("Enter values x1 for Line 1");
-            int x1 = Convert.ToInt32(Console.ReadLine());
+            x1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter values y1 for Line 1");
-            int y1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter values x2 and y2 for Line 1");
             Console.WriteLine("Enter values x2 for Line 1");
-            int x2 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter values y2 for Line 1");
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
 
 
             Console.WriteLine("Enter values x3 and y3 for Line 2");
             Console.WriteLine("Enter values x3 for Line 2");
-            int x3 = Convert.ToInt32(Console.ReadLine());
+            x3 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter values y3 for Line 2");
-            int y3 = Convert.ToInt32(Console.ReadLine());
+            y3 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter values x4 and y4 for Line 2");
             Console.WriteLine("Enter values x4 for Line 2");
-            int x4 = Convert.ToInt32(Console.ReadLine());
+            x4 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter values y4 for Line 2");
-            int y4 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void CompareLine()
+        {
             double length_of_line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             double length_of_line2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
 
@@ -44,11 +61,17 @@ namespace LineComparison
             else
                 Console.WriteLine("Line1 is smaller in length than Line2");
 
-            return line_difference;
         }
+
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-            Compare();
+            Line obj = new Line();
+            obj.Input();
+            obj.CompareLine();
+
         }
     }
 }
